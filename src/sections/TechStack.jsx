d71513edc63e1f38cb83819518c2c54fe68d65a3
elisha <korosenseimark40@gmail.com>
2025-09-26 22:05:27 +0600

    aking animation delay for techStack cards

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,9 +1,30 @@
 
+import { useGSAP } from "@gsap/react";
 import TitleHeader from "../components/TitleHeader";
 import TechIconCardExperience from "../components/models/TechLogos/TechIconCardExperience.jsx";
 import { techStackIcons } from "../constants";
+import { gsap } from "gsap";
+
+
+
 
 const TechStack = () => {
+  useGSAP(() => {
+    gsap.fromTo('.tech-card', { y: 50, opacity: 0 }, {
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      ease: 'power2.inOut',
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: '#skills',
+        start: 'top center',
+      }
+
+    })
+  })
+
+
   return (
     <div id="skills" className="flex-center section-padding">
       <div className="w-full h-full md:px-10 px-5">
