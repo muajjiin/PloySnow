36aa3128038e8a246d2015e329fb18ee093c5460
elisha <korosenseimark40@gmail.com>
2025-09-26 12:31:11 +0600

    making the experience

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,27 +1,63 @@
-import React from 'react'
 import TitleHeader from '../components/TitleHeader'
 import GlowCard from '../components/GlowCard'
 import { expCards } from '../constants'
 const Experience = () => {
   return (
-    <section id='exprience' className=' w-full md:mt-40 mt-20 section-padding xl:px-0'>
-      <div className='w-full h-full md:px-20 px-5'>
-        <TitleHeader title='Professional Work Experience' sub='My Carrer Overview' />
-        <div className='mt-32 relative'>
-          <div className=' relativ z-50 sl:space-y-32 space-y-10'>
+    <section
+      id="experience"
+      className="flex-center md:mt-40 mt-20 section-padding xl:px-0"
+    >
+      <div className="w-full h-full md:px-20 px-5">
+        <TitleHeader
+          title="Professional Work Experience"
+          sub="💼 My Career Overview"
+        />
+        <div className="mt-32 relative">
+          <div className="relative z-50 xl:space-y-32 space-y-10">
             {expCards.map((card) => (
-              <div key={card.title} className='exp-card-wrapper'>
-                {card.title}
-                <div className='sl:w-2/6'>
+              <div key={card.title} className="exp-card-wrapper">
+                <div className="xl:w-2/6">
                   <GlowCard card={card}>
-                    <img src={card.imgPath} alt={card.title} />
+                    <div>
+                      <img src={card.imgPath} alt="exp-img" />
+                    </div>
                   </GlowCard>
+                </div>
+                <div className="xl:w-4/6">
+                  <div className="flex items-start">
+                    <div className="timeline-wrapper">
+                      <div className="timeline" />
+                      <div className="gradient-line w-1 h-full" />
+                    </div>
+                    <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
+                      <div className="timeline-logo">
+                        <img src={card.logoPath} alt="logo" />
+                      </div>
+                      <div>
+                        <h1 className="font-semibold text-3xl">{card.title}</h1>
+                        <p className="my-5 text-white-50">
+                          🗓️&nbsp;{card.date}
+                        </p>
+                        <p className="text-[#839CB5] italic">
+                          Responsibilities
+                        </p>
+                        <ul className="list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50">
+                          {card.responsibilities.map(
+                            (responsibility, index) => (
+                              <li key={index} className="text-lg">
+                                {responsibility}
+                              </li>
+                            )
+                          )}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             ))}
           </div>
         </div>
-
       </div>
     </section>
   )
