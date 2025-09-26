54f0e9623fea01d8fcf0155a5530da467228df3c
elisha <korosenseimark40@gmail.com>
2025-09-26 23:56:25 +0600

    aking email and 3d seciton

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Hero from './sections/Hero.jsx'
-import ShowcaseSection from './sections/ShowcaseSection.jsx'
-import NavBar from './components/NavBar.jsx'
-import LogoShowcase from './sections/LogoShowcase.jsx'
-import FeatureCards from './sections/FeatureCards.jsx'
-import Experience from './sections/Experience.jsx'
-import TechStack from './sections/TechStack.jsx'
-import Testimonials from './sections/Testimonials.jsx'
+
+import Hero from './sections/Hero.jsx';
+import ShowcaseSection from './sections/ShowcaseSection.jsx';
+import NavBar from './components/NavBar.jsx';
+import LogoShowcase from './sections/LogoShowcase.jsx';
+import FeatureCards from './sections/FeatureCards.jsx';
+import Experience from './sections/Experience.jsx';
+import TechStack from './sections/TechStack.jsx';
+import Testimonials from './sections/Testimonials.jsx';
+import ContactExperience from './sections/Contact.jsx';
+
 const App = () => {
   return (
     <>
@@ -16,9 +19,10 @@ const App = () => {
       {/* <FeatureCards /> */}
       {/* <Experience /> */}
       {/* <TechStack /> */}
-      <Testimonials />
+      {/* <Testimonials /> */}
+      <ContactExperience />
     </>
   )
 }
 
-export default App
+export default App;
